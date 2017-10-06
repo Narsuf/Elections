640d20f80eb3445b6ba1e88744b66119bf790152
Jorge <jorgedguezm@gmail.com>
2017-10-06 21:51:33 +0200

    Small changes

--- app/build/generated/source/r/debug/com/jorgedguezm/elections/R.java
+++ app/build/generated/source/r/debug/com/jorgedguezm/elections/R.java
@@ -2676,7 +2676,7 @@ containing a value of this type.
         public static final int action_mode_bar_stub=0x7f0d005c;
         public static final int action_mode_close_button=0x7f0d0041;
         public static final int action_text=0x7f0d009b;
-        public static final int actions=0x7f0d00a9;
+        public static final int actions=0x7f0d00a8;
         public static final int activity_chooser_view_content=0x7f0d0042;
         public static final int add=0x7f0d001c;
         public static final int alertTitle=0x7f0d004e;
@@ -2731,7 +2731,7 @@ containing a value of this type.
         public static final int home=0x7f0d0004;
         public static final int homeAsUp=0x7f0d0011;
         public static final int icon=0x7f0d0047;
-        public static final int icon_group=0x7f0d00aa;
+        public static final int icon_group=0x7f0d00a9;
         public static final int ifRoom=0x7f0d003a;
         public static final int image=0x7f0d0044;
         public static final int info=0x7f0d00a6;
@@ -2772,7 +2772,7 @@ containing a value of this type.
         public static final int progress_horizontal=0x7f0d0007;
         public static final int radio=0x7f0d0059;
         public static final int right=0x7f0d002b;
-        public static final int right_icon=0x7f0d00a8;
+        public static final int right_icon=0x7f0d00aa;
         public static final int right_side=0x7f0d00a3;
         public static final int screen=0x7f0d001e;
         public static final int scroll=0x7f0d001a;
