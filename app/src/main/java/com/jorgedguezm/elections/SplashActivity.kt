fe2be15ff5ece018528f13a2a11f6c26d2235b3c
Jorge <jorgedguezm@gmail.com>
2018-09-30 02:53:30 +0200

    First request working successfully

--- app/src/main/java/com/jorgedguezm/elections/SplashActivity.kt
+++ app/src/main/java/com/jorgedguezm/elections/SplashActivity.kt
@@ -1,31 +1,36 @@
 package com.jorgedguezm.elections
 
 import android.content.Context
-import android.content.DialogInterface
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.View
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 import com.bumptech.glide.Glide
 
 import kotlinx.android.synthetic.main.splash_main.*
 
 class SplashActivity : AppCompatActivity() {
-    val isFirstLaunch = "isFirstLaunch"
+    private val isFirstLaunch = "isFirstLaunch"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_main)
 
+        //Set fullscreen UI
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         supportActionBar?.hide()
 
-        if (getPreferences(Context.MODE_PRIVATE).getBoolean(isFirstLaunch, true)) {
+        if (getPreferences(Context.MODE_PRIVATE).getBoolean(isFirstLaunch, true))
             callDialog()
-        }
 
+        //Load GIF
         Glide.with(this).load(R.drawable.loading).into(viewGif)
     }
 
@@ -33,12 +38,12 @@ class SplashActivity : AppCompatActivity() {
         val dialog = AlertDialog.Builder(this)
         dialog.setMessage(resources.getString(R.string.content_needs_to_be_downloaded))
         dialog.setPositiveButton(resources.getString(R.string.download),
-                DialogInterface.OnClickListener { dialogInterface, _ ->
+                { dialogInterface, _ ->
                     dialogInterface.cancel()
                     downloadContent()
                 })
         dialog.setNegativeButton(resources.getString(R.string.not_now),
-                DialogInterface.OnClickListener { dialogInterface, _ ->
+                { dialogInterface, _ ->
                     dialogInterface.cancel()
                     finish()
                 })
@@ -46,7 +51,15 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun downloadContent() {
-        getPreferences(Context.MODE_PRIVATE)
-                .edit().putBoolean(isFirstLaunch, false).apply()
+        val url = "http://90.171.38.91:8000/general_information/"
+
+        var request = StringRequest(Request.Method.GET, url,
+                { response -> Log.d("Response", response)},
+                { error -> Log.e("Error", error.message)})
+
+        Volley.newRequestQueue(this).add(request)
+
+        /*getPreferences(Context.MODE_PRIVATE)
+                .edit().putBoolean(isFirstLaunch, false).apply()*/
     }
 }
