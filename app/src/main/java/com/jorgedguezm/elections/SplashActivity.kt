4da45288f4ac6ad24725b1c9ec9ad2d0f6008312
Jorge <jorgedguezm@gmail.com>
2018-12-13 18:29:34 +0100

    Splash Activity refactored

--- app/src/main/java/com/jorgedguezm/elections/SplashActivity.kt
+++ app/src/main/java/com/jorgedguezm/elections/SplashActivity.kt
@@ -1,22 +1,12 @@
 package com.jorgedguezm.elections
 
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.View
 
-import com.android.volley.Request
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
-
-import com.bumptech.glide.Glide
-
-import kotlinx.android.synthetic.main.splash_main.*
-
 class SplashActivity : AppCompatActivity() {
-    private val isFirstLaunch = "isFirstLaunch"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,43 +14,24 @@ class SplashActivity : AppCompatActivity() {
 
         // Set fullscreen UI
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        supportActionBar?.hide()
 
-        if (getPreferences(Context.MODE_PRIVATE).getBoolean(isFirstLaunch, true))
-            callDialog()
-
-        // Load GIF
-        Glide.with(this).load(R.drawable.loading).into(viewGif)
+        // TODO Check if there is connection to the Internet
+        if (true) {
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        } else {
+            callErrorDialog()
+        }
     }
 
-    private fun callDialog() {
+    private fun callErrorDialog() {
         val dialog = AlertDialog.Builder(this)
-        dialog.setMessage(resources.getString(R.string.content_needs_to_be_downloaded))
-        dialog.setPositiveButton(resources.getString(R.string.download),
-                { dialogInterface, _ ->
-                    dialogInterface.cancel()
-                    downloadContent()
-                })
-        dialog.setNegativeButton(resources.getString(R.string.not_now),
-                { dialogInterface, _ ->
-                    dialogInterface.cancel()
-                    finish()
-                })
+        dialog.setTitle(resources.getString(R.string.no_internet_connection))
+        dialog.setMessage(resources.getString(R.string.application_will_be_closed))
+        dialog.setPositiveButton(resources.getString(R.string.ok)) { _, _ ->
+            finish()
+        }
         dialog.create().show()
-    }
-
-    private fun downloadContent() {
-        val url = "http://90.171.38.91:8000/general_information/"
-
-        val request = StringRequest(Request.Method.GET, url,
-                { response ->
-                    // TODO
-                },
-                { error -> Log.e("Request failed", error.message)})
-
-        Volley.newRequestQueue(this).add(request)
-
-        /*getPreferences(Context.MODE_PRIVATE)
-                .edit().putBoolean(isFirstLaunch, false).apply()*/
     }
 }
