57f765915d93866ef0eca55ab5c97debd9169e72
Narsuf <jorgedguezm@gmail.com>
2019-02-25 18:06:29 +0100

    Internet connection checked

--- app/src/main/java/com/jorgedguezm/elections/data/source/ElectionRepository.kt
+++ app/src/main/java/com/jorgedguezm/elections/data/source/ElectionRepository.kt
@@ -3,18 +3,23 @@ package com.jorgedguezm.elections.data.source
 import com.jorgedguezm.elections.data.Election
 import com.jorgedguezm.elections.data.source.local.ElectionsDao
 import com.jorgedguezm.elections.data.source.remote.ApiInterface
+import com.jorgedguezm.elections.utils.Utils
 
 import io.reactivex.Observable
 
 import javax.inject.Inject
 
 class ElectionRepository @Inject constructor(val apiInterface: ApiInterface,
-                                             val electionsDao: ElectionsDao) {
+                                             val electionsDao: ElectionsDao, val utils: Utils) {
 
     fun getElections(): Observable<List<Election>> {
-        val observableFromApi = getElectionsFromApi()
         val observableFromDb = getElectionsFromDb()
-        return Observable.concatArrayEager(observableFromApi, observableFromDb)
+        var returnValue = observableFromDb
+
+        if (utils.isConnectedToInternet())
+            returnValue = Observable.concatArrayEager(getElectionsFromApi(), observableFromDb)
+
+        return returnValue
     }
 
     fun getElectionsFromApi(): Observable<List<Election>> {
