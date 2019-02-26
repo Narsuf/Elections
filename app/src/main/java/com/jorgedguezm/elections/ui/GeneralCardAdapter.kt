266103b92bd23c281c52817d27525f6d5fda5b26
Narsuf <jorgedguezm@gmail.com>
2019-02-26 12:00:23 +0100

    Cards shown

---
+++ app/src/main/java/com/jorgedguezm/elections/ui/GeneralCardAdapter.kt
@@ -0,0 +1,41 @@
+package com.jorgedguezm.elections.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+import com.jorgedguezm.elections.R
+import com.jorgedguezm.elections.data.Election
+
+import kotlinx.android.synthetic.main.general_elections_card.view.*
+
+class GeneralCardAdapter(private val elections: Array<Election>) :
+        RecyclerView.Adapter<GeneralCardAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(val card: CardView) : RecyclerView.ViewHolder(card)
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): GeneralCardAdapter.MyViewHolder {
+        // create a new view
+        val card = LayoutInflater.from(parent.context)
+                .inflate(R.layout.general_elections_card, parent, false) as CardView
+        // set the view's size, margins, paddings and layout parameters
+        return MyViewHolder(card)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.card.section_label.text = elections[position].year.toString()
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = elections.size
+}
--- app/src/main/java/com/jorgedguezm/elections/ui/MainFragment.kt
+++ app/src/main/java/com/jorgedguezm/elections/ui/MainFragment.kt
@@ -1,24 +1,27 @@
 package com.jorgedguezm.elections.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 import com.jorgedguezm.elections.R
 import com.jorgedguezm.elections.data.Election
 
 import dagger.android.support.AndroidSupportInjection
 
-import kotlinx.android.synthetic.main.fragment_main.view.*
+import kotlinx.android.synthetic.main.fragment_main.*
 
 import javax.inject.Inject
 
 class MainFragment : Fragment() {
+
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
 
     @Inject
     lateinit var electionsViewModelFactory: ElectionsViewModelFactory
@@ -55,20 +58,40 @@ class MainFragment : Fragment() {
         electionsViewModel.loadElections()
         electionsViewModel.electionsResult().observe(this,
                 Observer<List<Election>> {
-                    Log.d("Elections", it.toString())
-                })
-
-        electionsViewModel.electionsError().observe(this,
-                Observer<String> {
-                    Log.d("Error", it)
+                    when (arguments?.getInt(ARG_SECTION_NUMBER)) {
+                        1 -> createCards(it)
+                    }
                 })
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        val rootView = inflater.inflate(R.layout.fragment_main, container, false)
-        rootView.section_label.text = getString(R.string.section_format,
-                arguments?.getInt(ARG_SECTION_NUMBER))
-        return rootView
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onDestroy() {
+        electionsViewModel.disposeElements()
+        super.onDestroy()
+    }
+
+    private fun createCards(elections: List<Election>) {
+        val generalElections = ArrayList<Election>()
+
+        for (e in elections)
+            if (e.name.equals("Generales")) generalElections.add(e)
+
+        viewAdapter = GeneralCardAdapter(generalElections.toTypedArray())
+
+        recyclerView.apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = LinearLayoutManager(context)
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+        }
     }
 }
