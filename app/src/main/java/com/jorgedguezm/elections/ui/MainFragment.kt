c8b558c847900a7d05250085e9590fe3f405dadc
Jorge <jorgedguezm@gmail.com>
2019-02-28 20:50:51 +0100

    Unused import deleted

--- app/src/main/java/com/jorgedguezm/elections/ui/MainFragment.kt
+++ app/src/main/java/com/jorgedguezm/elections/ui/MainFragment.kt
@@ -2,7 +2,6 @@ package com.jorgedguezm.elections.ui
 
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
