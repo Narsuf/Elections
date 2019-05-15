5b566b3fd4a3a0460c80d700afd2e8381359fa57
Narsuf <jorgedguezm@gmail.com>
2019-05-15 17:05:29 +0200

    First test added

---
+++ app/src/test/java/com/jorgedguezm/elections/ElectionUtils.kt
@@ -0,0 +1,12 @@
+package com.jorgedguezm.elections
+
+import com.jorgedguezm.elections.data.Party
+
+class ElectionUtils {
+
+    companion object {
+        fun generateParty() : Party {
+            return Party("PACMA", "#ADBE18")
+        }
+    }
+}
