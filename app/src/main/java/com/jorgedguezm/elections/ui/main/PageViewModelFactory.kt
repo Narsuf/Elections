7c517d6527f5f78b46180d5c40dacb36880e8778
Jorge-Ubuntu <jorgedguezm@gmail.com>
2019-10-18 12:57:27 +0200

    Indentation

--- app/src/main/java/com/jorgedguezm/elections/ui/main/PageViewModelFactory.kt
+++ app/src/main/java/com/jorgedguezm/elections/ui/main/PageViewModelFactory.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.ViewModelProvider
 
 import javax.inject.Inject
 
-class PageViewModelFactory @Inject constructor(private val pageViewModel: PageViewModel) :
-        ViewModelProvider.Factory {
+class PageViewModelFactory @Inject constructor(
+        private val pageViewModel: PageViewModel) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(PageViewModel::class.java))
