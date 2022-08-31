8242fe8f36f1981c0122628928d4ef485db5c688
Jorge <jorgedguezm@gmail.com>
2022-08-31 14:05:01 +0200

    Tests added

--- app/src/androidTest/java/com/jorgedguezm/elections/presentation/main/MainActivityUITest.kt
+++ app/src/androidTest/java/com/jorgedguezm/elections/presentation/main/MainActivityUITest.kt
@@ -3,6 +3,8 @@ package com.jorgedguezm.elections.presentation.main
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import com.adevinta.android.barista.assertion.BaristaVisibilityAssertions.assertNotDisplayed
 import com.adevinta.android.barista.interaction.BaristaListInteractions.clickListItem
 import com.jorgedguezm.elections.R
 import com.jorgedguezm.elections.presentation.detail.DetailActivity
@@ -20,6 +22,9 @@ class MainActivityUITest {
 
         assertToolbarTitle("Elections")
         sleep(3000)
+        assertNotDisplayed(R.id.error_animation)
+        assertNotDisplayed(R.id.loading_animation)
+        assertDisplayed(R.id.recyclerView)
         intents {
             clickListItem(R.id.recyclerView, 0)
             intended(allOf(hasComponent(DetailActivity::class.java.name)))
