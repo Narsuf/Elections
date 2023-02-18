50188523ea75c44f6f85520e0b6b88d427b77447
Jorge <jorgedguezm@gmail.com>
2023-02-18 15:50:19 +0100

    Stop tracking Analytics when debugging

--- app/src/main/java/com/n27/elections/presentation/common/extensions/FirebaseAnalyticsExtensions.kt
+++ app/src/main/java/com/n27/elections/presentation/common/extensions/FirebaseAnalyticsExtensions.kt
@@ -1,8 +1,9 @@
 package com.n27.elections.presentation.common.extensions
 
+import androidx.multidex.BuildConfig
 import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.analytics.ktx.logEvent
 
 fun FirebaseAnalytics.track(eventName: String, paramId: String, paramValue: String) {
-    logEvent(eventName) { param(paramId, paramValue) }
+    if (!BuildConfig.DEBUG) logEvent(eventName) { param(paramId, paramValue) }
 }
