5e20aa541fc4d1b9039dca0bdc4d7c2a5917465f
Narsuf <jorgedguezm@gmail.com>
2023-02-23 13:16:43 +0100

    Flakyness reduced

--- app/src/androidTest/java/com/n27/elections/presentation/main/MainActivityUITest.kt
+++ app/src/androidTest/java/com/n27/elections/presentation/main/MainActivityUITest.kt
@@ -112,7 +112,7 @@ class MainActivityUITest {
         }
 
         clickOn("CLOSE")
-        sleep(10000)
+        sleep(7000)
         assertToolbarTitle("Elections")
         assertNotDisplayed(R.id.error_animation)
         assertNotDisplayed(R.id.loading_animation)
