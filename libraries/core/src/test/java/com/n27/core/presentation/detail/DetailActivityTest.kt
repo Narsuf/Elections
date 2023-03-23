78ad850bc6a7e28598dba563db796991e06bef29
Jorge <jorgedguezm@gmail.com>
2023-03-23 18:37:53 +0100

    Tests fixed@

--- libraries/core/src/test/java/com/n27/core/presentation/detail/DetailActivityTest.kt
+++ libraries/core/src/test/java/com/n27/core/presentation/detail/DetailActivityTest.kt
@@ -3,7 +3,7 @@ package com.n27.core.presentation.detail
 import android.content.Intent
 import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider
-import com.n27.core.Constants.KEY_CONGRESS_ELECTION
+import com.n27.core.Constants.KEY_ELECTION
 import com.n27.core.Constants.KEY_SENATE
 import com.n27.core.Constants.KEY_SENATE_ELECTION
 import com.n27.test.generators.getElection
@@ -21,30 +21,30 @@ class DetailActivityTest {
     @Test
     fun launchDetailActivity() {
         launchActivity().onActivity { activity ->
-            // Congress results loaded
-            activity.checkCongress()
+            with(activity) {
+                // Congress results loaded
+                checkCongress()
 
-            // Swap option clicked
-            activity.onOptionsItemSelected(activity.binding.toolbar.menu.getItem(0))
-            activity.checkSenate()
+                // Swap option clicked
+                onOptionsItemSelected(binding.toolbar.menu.getItem(0))
+                checkSenate()
 
-            // Swap option clicked again
-            activity.onOptionsItemSelected(activity.binding.toolbar.menu.getItem(0))
-            activity.checkCongress()
+                // Swap option clicked again
+                onOptionsItemSelected(binding.toolbar.menu.getItem(0))
+                checkCongress()
 
-            val fragment = activity.supportFragmentManager.fragments[0] as DetailFragment
+                // Check highlight function
+                binding.listView.onItemClickListener?.onItemClick(null, null, 0, 0)
+                assertTrue(binding.pieChart.highlighted.isNotEmpty())
 
-            // Check highlight function
-            fragment.binding.listView.onItemClickListener?.onItemClick(null, null, 0, 0)
-            assertTrue(fragment.binding.pieChart.highlighted.isNotEmpty())
+                // Still highlighted
+                countDownTimer.onTick(1)
+                assertTrue(binding.pieChart.highlighted.isNotEmpty())
 
-            // Still highlighted
-            fragment.countDownTimer.onTick(1)
-            assertTrue(fragment.binding.pieChart.highlighted.isNotEmpty())
-
-            // Not highlighted anymore
-            fragment.countDownTimer.onFinish()
-            assertNull(fragment.binding.pieChart.highlighted)
+                // Not highlighted anymore
+                countDownTimer.onFinish()
+                assertNull(binding.pieChart.highlighted)
+            }
         }
     }
 
@@ -60,7 +60,7 @@ class DetailActivityTest {
 
     private fun launchActivity() = ActivityScenario.launch<DetailActivity>(
         Intent(ApplicationProvider.getApplicationContext(), DetailActivity::class.java).apply {
-            putExtra(KEY_CONGRESS_ELECTION, congressElection)
+            putExtra(KEY_ELECTION, congressElection)
             putExtra(KEY_SENATE_ELECTION, senateElection)
         }
     )
--- libraries/test/src/main/java/com/n27/test/TestApplication.kt
+++ libraries/test/src/main/java/com/n27/test/TestApplication.kt
@@ -1,17 +1,23 @@
 package com.n27.test
 
+import android.app.Application
 import androidx.multidex.MultiDexApplication
+import androidx.room.Room
+import com.n27.core.data.room.Database
 import com.n27.core.presentation.common.PresentationUtils
 import com.n27.core.presentation.injection.DetailComponent
 import com.n27.core.presentation.injection.DetailComponentProvider
 import dagger.Component
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import javax.inject.Singleton
 
 class TestApplication : MultiDexApplication(), DetailComponentProvider {
 
-    override fun provideDetailComponent(): TestApplicationComponent = DaggerTestApplicationComponent.create()
+    override fun provideDetailComponent(): TestApplicationComponent = DaggerTestApplicationComponent.builder()
+        .fakeModule(FakeModule(this))
+        .build()
 }
 
 @Singleton
@@ -19,9 +25,28 @@ class TestApplication : MultiDexApplication(), DetailComponentProvider {
 interface TestApplicationComponent : DetailComponent
 
 @Module
-class FakeModule {
+class FakeModule(val app: Application) {
 
     @Provides
     @Singleton
     fun providePresentationUtils(): PresentationUtils = PresentationUtils()
+
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(): OkHttpClient = OkHttpClient.Builder().build()
+
+    @Provides
+    @Singleton
+    fun provideDatabase(app: Application) = Room
+        .databaseBuilder(app, Database::class.java, "elections_db")
+        .fallbackToDestructiveMigration()
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideElectionDao(database: Database) = database.electionDao()
+
+    @Provides
+    @Singleton
+    fun provideApplication() = app
 }
