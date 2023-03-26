68e63529c91ec169535febef144d1e21f3c15f4a
Jorge <jorgedguezm@gmail.com>
2023-03-26 20:34:48 +0200

    All provinces added

--- libraries/core/src/main/java/com/n27/core/data/RegionalLiveRepository.kt
+++ libraries/core/src/main/java/com/n27/core/data/RegionalLiveRepository.kt
@@ -34,7 +34,7 @@ class RegionalLiveRepository @Inject constructor(
         val provinceRegions = JSONObject(loadJSON("provinces.json"))
             .getJSONArray("provinces")
 
-        for (z in 8 until 12) {
+        for (z in 18 until provinceRegions.length()) {
             val region = provinceRegions.getJSONObject(z)
             val provinces = region.getJSONArray(region.names()!!.getString(0))
             val regionId = regions.getJSONObject(z).getString("id")
