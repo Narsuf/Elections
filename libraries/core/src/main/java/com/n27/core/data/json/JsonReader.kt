641c1745011b14f341d8d5e2438d193082aa051f
Jorge <jorgedguezm@gmail.com>
2023-03-27 13:34:13 +0200

    Small refactor

--- libraries/core/src/main/java/com/n27/core/data/json/JsonReader.kt
+++ libraries/core/src/main/java/com/n27/core/data/json/JsonReader.kt
@@ -10,12 +10,13 @@ import javax.inject.Singleton
 @Singleton
 class JsonReader {
 
-    suspend fun getStringJson(res: String) = withContext(Dispatchers.IO) {
-        runCatching {
+    suspend fun getStringJson(res: String) = runCatching {
+        withContext(Dispatchers.IO) {
             val inputStream = javaClass.classLoader!!.getResourceAsStream(res)
             val buffer = inputStream.source().buffer()
             buffer.readString(StandardCharsets.UTF_8).apply { inputStream.close() }
-        }.getOrElse { "" }
-    }
+        }
+    }.getOrElse { "" }
+
 }
 
